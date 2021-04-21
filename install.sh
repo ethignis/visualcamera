734ed27533d761f79428fbcbfda9d83496d8fda2
bizoffermark <bizoffermark@gmail.com>
2021-04-21 18:11:48 +0200

    Install updated

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "Check the dependency on spinview!"
+
+if ! command -v spinview &> /dev/null
+then
+    echo "Spinview could not be found"
+    echo "Please first download the Spinnaker SDK from https://www.flir.de/products/spinnaker-sdk/"
+    exit
+fi
+
 echo "Install dependencies"
 sudo apt install libunwind8-dev
 
